5677a0025ac02354179e10b37bbe8eb2dbc6c4ff
rf777rf777 <rf777rf777@gmail.com>
2018-03-20 23:34:27 +0800

    edit app.py

--- app.py
+++ app.py
@@ -3,46 +3,63 @@ import simplekml
 import sys
 import os
 
-application_path = os.path.dirname(sys.executable)
+def findXlsxFilePath(appPath):	
+	fileName = ''
+	for file in os.listdir(appPath):
+		if (file.lower()).endswith('xlsx'):
+			fileName = file
+	path = appPath + '/{}'.format(fileName)
 
-fileName = ''
-#巡覽照片資料夾
-for file in os.listdir(application_path):
-    if (file.lower()).endswith('xlsx'):
-        fileName = file
-path = application_path+'/'+fileName
-excel = pandas.ExcelFile(path)
+	return path
 
-sheetNamesList = excel.sheet_names 
-#df = pandas.read_excel("屏東_已編輯.xlsx").fillna('')
+def createKML(filePath):
+	excel = pandas.ExcelFile(filePath)
+	sheetNamesList = excel.sheet_names
+	kml = simplekml.Kml()
+	doc = kml.newdocument(name = "Test")
+	layers = []
+	
+	for sheet_name in sheetNamesList:
+		layers.append(doc.newfolder(name= sheet_name))
 
-kml = simplekml.Kml()
-doc = kml.newdocument(name = fileName)
-layers = []
-#for i in range(len(位置集合)):
-    #fld.append(doc.newfolder(name = 位置集合[i]))
-for sheet_name in sheetNamesList:
-	layers.append(doc.newfolder(name= sheet_name))
-'''
-for item in layers:
-	print(type(item))
-pass
-'''
-sharedstyle = simplekml.Style()
-sharedstyle.labelstyle.scale = 0.85
-sharedstyle.iconstyle.color = 'fffff8f0'
-sharedstyle.iconstyle.scale = 1
-sharedstyle.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png'
 
-#dataList = []
+	sharedstyle = simplekml.Style()
+	sharedstyle.labelstyle.scale = 0.85
+	sharedstyle.iconstyle.scale = 1
+	sharedstyle.iconstyle.icon.href = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png'
 
-pnt = []
-for layersItem in layers:
-	data = excel.parse(layersItem.name).fillna('')
-	for i in range(len(data)):
-		point = layersItem.newpoint(name = data.ix[i]['站名'],
-							 		description = data.ix[i]['所在鄉鎮']	,	
-							 		coords = [(data.ix[i]['E'],data.ix[i]['N'])])
-		
+	pnt = []
+	complete = 0
+	for layersItem in layers:
+		data = excel.parse(layersItem.name).fillna('')
+		for i in range(len(data)):
+			point = layersItem.newpoint(name = data.ix[i]['站名'],
+								 		description = data.ix[i]['所在鄉鎮']	,	
+								 		coords = [(data.ix[i]['E'],data.ix[i]['N'])])
+		complete += 1
+		completeInTerminal(complete,len(layers))
+	return kml
 
-kml.save(application_path+"/"+'{}.kml'.format(fileName.split('.')[0]))
+def completeInTerminal(complete,total):
+	done = int(30 * complete / total)
+	sys.stdout.write("\rCompleted：[{0}{1}] {2}%".format('#'*done, '-'*(30 - done) , int(complete/total*100)))
+	sys.stdout.flush()
+
+def main():
+	#if執行exe檔
+	if getattr(sys, 'frozen', False):
+		filePath = findXlsxFilePath(os.path.dirname(sys.executable))
+	#if直接執行.py檔
+	elif __file__:
+		filePath = findXlsxFilePath(os.path.abspath(os.path.dirname(__file__)))
+	try:
+		kml = createKML(filePath)		
+		kml.save('{}.kml'.format(filePath.split('.')[0]))		
+	except Exception as e:
+		print('發生例外狀況：{0} \n程式停止！'.format(e))
+
+
+if __name__ == "__main__":
+    main()
+    if os.name == 'nt':
+    	input("\n<<< 按任意鍵關閉視窗 >>>")
